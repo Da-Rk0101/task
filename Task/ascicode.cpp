4fa963c3ab80bb012d82c0993bf5482d199ed431
Da-Rk0101 <ahmedshetat01@gmail.com>
2024-05-08 11:57:45 +0000

    this is my task

---
+++ Task/ascicode.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a[20] = {};
+    cin.getline(a,5);
+   // cout<<a[0]<<endl;
+     //cout<<a[1]<<endl;
+     //cout<<a[2]<<endl;
+     cout<<a[3]<<endl;
+     cout<<a[4]<<endl;
+    
+    //for(int i=0; i<3; i++){
+        // x = a[i];
+      //  cout<<x<<" ";
+    //}
+}
